d09648e11709c4e2bb05536959b7b6596fb365d6
Jimmy Russel Kurniawan <80058599+jimmyrk@users.noreply.github.com>
2024-03-08 22:40:54 +0700

    update

--- cypress/e2e/api/pokeapi/get.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Get List Users', () => {
-    it('verify the user list will displayed', () => {
-        cy.request({
-            method: 'GET',
-            url: 'https://pokeapi.co/api/v2/pokemon/ditto',
-        }).as('users')
-        cy.get('@users').its('status').should('equal', 200)
-    });
-});
--- cypress/e2e/api/pokeapi/header.cy.js
+++ cypress/e2e/api/pokeapi/header.cy.js
@@ -1,7 +1,15 @@
-describe('Validate Header', () => {
-    it('Successfully validate content-type', () => {
-        cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
+describe('Validate Header and Response Body', () => {
+    it('Successfully validate content-type and response body', () => {
+        // Lakukan permintaan ke API untuk mendapatkan informasi tentang Pokemon Ditto
+        cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon');
+
+        // Assertion untuk validasi content-type pada header
         cy.get('@pokemon').its('headers').its('content-type')
-            .should('include', 'application/json; charset=utf-8')
+            .should('include', 'application/json; charset=utf-8');
+
+        // Assertion untuk validasi response body
+        cy.get('@pokemon').its('body')
+            .should('have.property', 'name', 'ditto'); // Memastikan bahwa nama Pokemon adalah "ditto"
+        // Anda bisa menambahkan assertion lainnya sesuai dengan properti yang ingin Anda periksa pada response body
     });
 });
